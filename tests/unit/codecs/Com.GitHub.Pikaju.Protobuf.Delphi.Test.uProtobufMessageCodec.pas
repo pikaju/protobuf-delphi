24fc50f4c4a7e627419f517a2345dab8130771a7
Julien Scholz <julienscholzo@hotmail.de>
2020-09-08 21:12:15 +0200

    QV5-103 Add unit tests for message codec

--- tests/unit/codecs/Com.GitHub.Pikaju.Protobuf.Delphi.Test.uProtobufMessageCodec.pas
+++ tests/unit/codecs/Com.GitHub.Pikaju.Protobuf.Delphi.Test.uProtobufMessageCodec.pas
@@ -11,9 +11,13 @@ uses
   Generics.Collections,
   Sysutils,
   Com.GitHub.Pikaju.Protobuf.Delphi.uProtobufMessageCodec,
+  Com.GitHub.Pikaju.Protobuf.Delphi.uProtobufMessage,
   Com.GitHub.Pikaju.Protobuf.Delphi.uProtobufTag,
   Com.GitHub.Pikaju.Protobuf.Delphi.uProtobufEncodedField,
   Com.GitHub.Pikaju.Protobuf.Delphi.Test.uProtobufTestUtility;
+
+type
+  TEmpty = class(TProtobufMessage);
 
 procedure TestMessageCodec;
 
@@ -22,10 +26,18 @@ implementation
 procedure TestMessageEncoding;
 var
   lStream: TMemoryStream;
+  lEmpty: TEmpty;
+  lEmptyCodec: TProtobufMessageWireCodec<TEmpty>;
 begin
   lStream := TMemoryStream.Create;
 
-  
+  lEmpty := TEmpty.Create;
+  lEmptyCodec := TProtobufMessageWireCodec<TEmpty>.Create; 
+  lEmptyCodec.EncodeField(5, lEmpty, lStream);
+  lEmptyCodec.Free;
+  lEmpty.Free;
+  AssertStreamEquals(lStream, [5 shl 3 or 2, 0], 'Encoding an empty message works');
+  lStream.Clear; 
 
   lStream.Free;
 end;
@@ -33,10 +45,14 @@ end;
 procedure TestMessageDecoding;
 var
   aList: TList<TProtobufEncodedField>;
-  lInt32: Int32;
+  lEmptyCodec: TProtobufMessageWireCodec<TEmpty>;
 begin
   aList := TObjectList<TProtobufEncodedField>.Create;
   
+  aList.Add(TProtobufEncodedField.CreateWithData(TProtobufTag.Create(5, wtLengthDelimited), [0]));
+  lEmptyCodec := TProtobufMessageWireCodec<TEmpty>.Create; 
+  lEmptyCodec.DecodeField(aList).Free;
+  lEmptyCodec.Free;
 
   aList.Free;
 end;
