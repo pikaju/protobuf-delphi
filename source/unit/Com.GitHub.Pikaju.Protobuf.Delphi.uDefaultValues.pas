973009e9079c21725250bbfacf33f611af4939b0
Julien Scholz <julienscholzo@hotmail.de>
2020-09-08 08:36:12 +0200

    QV5-103 Add default values file

---
+++ source/unit/Com.GitHub.Pikaju.Protobuf.Delphi.uDefaultValues.pas
@@ -0,0 +1,22 @@
+unit Com.GitHub.Pikaju.Protobuf.Delphi.uDefaultValues;
+
+{$IFDEF FPC}
+  {$MODE DELPHI}
+{$ENDIF}
+
+interface
+
+type
+
+const
+  // Protobuf default values. See: https://developers.google.com/protocol-buffers/docs/proto3#default.
+
+  PROTOBUF_DEFAULT_VALUE_STRING: UnicodeString = '';
+  PROTOBUF_DEFAULT_VALUE_BOOL = False;
+  PROTOBUF_DEFAULT_VALUE_NUMERIC = 0;
+  PROTOBUF_DEFAULT_VALUE_ENUM = 0;
+  PROTOBUF_DEFAULT_VALUE_MESSAGE = nil;
+
+implementation
+
+end.
